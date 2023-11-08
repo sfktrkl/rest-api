3bbcba75c8306b6d131d023b026b837afeb59c40
Safak Turkeli <sfktrkl@gmail.com>
2023-11-08 04:29:32 +0300

    Create a middleware for errors

---
+++ middlewares/errors.js
@@ -0,0 +1,8 @@
+export function error(err, req, res, next) {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+}
