fc7748dc6c63b990ac4efb9debd79fad9cc1a5f8
Safak Turkeli <sfktrkl@gmail.com>
2023-11-10 04:39:39 +0300

    Generate forgot password token

--- models/users.js
+++ models/users.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,6 +49,16 @@ userSchema.methods.getJWTToken = function () {
 
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+  return resetToken;
 };
 
 export default mongoose.model("User", userSchema);
