e972ad4b4b4dbbc01f2a8307311ef439443c922f
Safak Turkeli <sfktrkl@gmail.com>
2023-11-11 23:50:40 +0300

    Update user data

--- controllers/profile.js
+++ controllers/profile.js
@@ -26,3 +26,21 @@ export const updatePassword = catchErrors(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+export const updateUser = catchErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getUserProfile, updatePassword } from "../controllers/profile.js";
+import {
+  getUserProfile,
+  updatePassword,
+  updateUser,
+} from "../controllers/profile.js";
 import { authenticationMiddleware } from "../middlewares/auth.js";
 
 const router = Router();
 router.route("/me").get(authenticationMiddleware, getUserProfile);
 router.route("/password/update").put(authenticationMiddleware, updatePassword);
+router.route("/me/update").put(authenticationMiddleware, updateUser);
 
 export default router;
