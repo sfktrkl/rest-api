fb735b58e683967be912cbb56c71c95e8fea46b4
Safak Turkeli <sfktrkl@gmail.com>
2023-11-11 23:47:04 +0300

    Update password

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,6 @@
 import User from "../models/users.js";
-import { catchErrors } from "../utils/errorHandler.js";
+import { sendToken } from "../utils/jwtToken.js";
+import { ErrorHandler, catchErrors } from "../utils/errorHandler.js";
 
 export const getUserProfile = catchErrors(async (req, res, next) => {
   const user = await User.findById(req.user.id);
@@ -8,4 +9,20 @@ export const getUserProfile = catchErrors(async (req, res, next) => {
     success: true,
     data: user,
   });
+});
+
+export const updatePassword = catchErrors(async (req, res, next) => {
+  const { currentPassword, newPassword } = req.body;
+  if (!currentPassword)
+    return next(new ErrorHandler("Enter current password.", 400));
+  if (!newPassword) return next(new ErrorHandler("Enter new password.", 400));
+
+  const user = await User.findById(req.user.id).select("+password");
+  const isMatch = await user.comparePassword(currentPassword);
+  if (!isMatch) return next(new ErrorHandler("Password is incorrect.", 401));
+
+  user.password = newPassword;
+  await user.save();
+
+  sendToken(user, 200, res);
 });
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUserProfile } from "../controllers/profile.js";
+import { getUserProfile, updatePassword } from "../controllers/profile.js";
 import { authenticationMiddleware } from "../middlewares/auth.js";
 
 const router = Router();
 router.route("/me").get(authenticationMiddleware, getUserProfile);
+router.route("/password/update").put(authenticationMiddleware, updatePassword);
 
 export default router;
